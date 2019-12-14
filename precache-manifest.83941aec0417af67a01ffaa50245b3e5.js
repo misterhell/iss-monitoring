c08693ad4d71e3dae7d2ce130382cf2df6c8d154
max <misterhell94@gmail.com>
2019-12-15 02:01:19 +0300

    deploy

---
+++ precache-manifest.83941aec0417af67a01ffaa50245b3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38e7818facf30f643ea393dda314692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef7c82a65dd7ec180f1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3e17a9c4ed444811fbf7",
+    "url": "/static/js/2.48ba65fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.48ba65fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eef7c82a65dd7ec180f1",
+    "url": "/static/js/main.8435d443.chunk.js"
+  },
+  {
+    "revision": "8ca467032f1a638a1cdf",
+    "url": "/static/js/runtime-main.c4fcecdf.js"
+  }
+]);

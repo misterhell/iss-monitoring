46f48217477d5a52847a66a195cba625300634ea
max <misterhell94@gmail.com>
2019-12-14 23:41:43 +0300

    iss replacing on map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { YMaps, Map, Placemark, FullscreenControl } from 'react-yandex-maps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    iss: {
+      latitude: 0,
+      longitude: 0
+    },
+
+    map: {
+      center: [0, 0],
+      zoom: 1,
+    }
+  }
+
+
+  fetchPosition() {
+    fetch('http://api.open-notify.org/iss-now.json', { mode: 'cors' })
+      .then(response => response.text())
+      .then(json => JSON.parse(json))
+      .then(({ iss_position }) => this.setState(state => (
+        {
+          ...state,
+          iss: { ...iss_position },
+          map: {
+            center: [iss_position.latitude, iss_position.longitude],
+            zoom: 3
+          }
+        })
+      ))
+  }
+
+  startFetchingPosition() {
+    this.fetchPosition()
+    setInterval(() => {
+      this.fetchPosition()
+    }, 5500)
+  }
+
+  componentDidMount() {
+    console.log(Map.behaviors)
+    this.startFetchingPosition()
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h3>
+            ISS current position
+        </h3>
+          <YMaps style={{ width: `400px`, height: `400px` }}>
+            <Map height={500} width={800}
+              state={{ ...this.state.map }}
+
+            >
+              <Placemark
+                geometry={[this.state.iss.latitude, this.state.iss.longitude]}
+                defaultOptions={{
+                  iconLayout: 'default#image',
+                  iconImageHref: '/iss-station-2-512.png',
+                  iconImageSize: [50, 50],
+                }}
+              />
+              <FullscreenControl />
+            </Map>
+          </YMaps>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
